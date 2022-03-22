12581ef8262abc13ba42cda8b59a06b35ea801f6
QuincyClain <pesotskiyvlad9@gmail.com>
2022-03-22 22:42:22 +0300

    correct

--- lab1/main.py
+++ lab1/main.py
@@ -1,23 +1,33 @@
 from statistics import median
 
+
 def average_count(text_in):
     sentence_count = 0
     total_words = 0
 
-    for key in text_in:
+    for check in text_in:
         sentence_count += 1
-        total_words += len(text_in[key])
+        total_words += len(text_in[check])
 
-    return (total_words / (sentence_count -1))
+    sentence_count -= 1
+    if sentence_count == 0:
+        sentence_count = 1
+    return total_words / sentence_count
 
 
 def median_count(text_in):
     median_arr = []
+    sentence_count = 0
 
+    for value in text_in.values():
+        sentence_count += 1
     for key in text_in:
         median_arr.append(len(text_in[key]))
+    val = median(median_arr)
+    if val == 0.5:
+        val += 0.5
 
-    return (median(median_arr))
+    return val
 
 
 def repeats(text_in):
@@ -32,25 +42,24 @@ def repeats(text_in):
     return list_words
 
 
-def top_ngrams(text_in, K, N):
+def top_ngrams(text_in, k, n):
     keys = []
 
     for value in text_in.values():
         for element in value:
             for letter in element:
                 keys.append(letter)
-            letters_sequences = [keys[i:] for i in range(N)]
-
+            letters_sequences = [keys[i:] for i in range(n)]
     ngrams_list = list(zip(*letters_sequences))
-    ngrams_dictionary = {}
 
+    ngrams_dictionary = {}
     for ngram in ngrams_list:
         if ngram in ngrams_dictionary:
             ngrams_dictionary[ngram] += 1
         else:
             ngrams_dictionary[ngram] = 1
 
-    print(f"Top-{K} {N}-grams:")
+    print(f"Top-{k} {n}-grams:")
 
     return (dict(sorted(ngrams_dictionary.items(), key=lambda item: item[1], reverse=True)))
 
@@ -64,7 +73,7 @@ def replace_all(text, dictionary):
 def main():
 
     text = input().lower()
-    correct_text = replace_all(text, {",": "", ":" : "", ";" : "", "?": ".", "!": "." }).split(".")
+    correct_text = replace_all(text, {",": "", ":" : "", ";" : "", "?": ".", "!": "."}).split(".")
     text_in = dict()
 
     for sentence in correct_text:
@@ -72,28 +81,36 @@ def main():
 
     list_words = repeats(text_in)
     print("Word repetition list:")
-
     for key in list_words:
         print(f"{key}:{list_words[key]}")
+
     average_count(text_in)
     print(f"Average amount of words in sentences:{average_count(text_in)}")
+
     median_count(text_in)
     print(f"Median amount of words in sentences:{median_count(text_in)}")
 
+    print("Do you want to change K = 10 or N = 4? yes/no")
+    inter_string = input()
+    if inter_string == 'yes' or inter_string == 'Yes':
+        print("Enter K value")
+        k = int(input())
+        print("Enter N value")
+        n = int(input())
+    elif inter_string == 'no' or inter_string == 'No':
+        k = 10
+        n = 4
 
-    K = 10
-    sort_ngram = top_ngrams(text_in, 10, 4)
+    sort_ngram = top_ngrams(text_in, k, n)
 
     for hey in sort_ngram:
-        K -= 1
-        if K == -1:
+        k -= 1
+        if k == -1:
             return
         for meow in hey:
             print(f"{meow}", end="")
-
         print(f":{sort_ngram[hey]}")
 
 
-        
 if __name__ == "__main__":
     main()
