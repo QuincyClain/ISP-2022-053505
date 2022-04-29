71fd62f845c0892970219b83e10efb855e8bab3b
Песоцкий Владислав <79206316+QuincyClain@users.noreply.github.com>
2022-04-29 15:03:58 +0300

    Add files via upload

---
+++ lab2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='my_parser',
+    version='1.0',
+    packages=['tests', 'parsers', 'my_factory', 'intermediate_parser'],
+    url='',
+    license='',
+    author='QuincyClain',
+    author_email='pesotskiyvlad9@gmail.com',
+    description='application to parse different objects'
+)
